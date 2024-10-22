40de99ded734620065280bae4269b83bb8354e16
chad-shimozaki <chad.shimozaki@chicagobooth.edu>
2024-10-22 20:57:02 +0000

    all erbs

--- dice.rb
+++ dice.rb
@@ -20,10 +20,10 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six)
+
 end
 
 get("/dice/2/10") do
@@ -31,19 +31,19 @@ get("/dice/2/10") do
   second_die = rand(1..10)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+  erb(:two_ten)
+  
 end
 
 get("/dice/1/20") do
   first_die = rand(1..20)
 
-  outcome = "You rolled a #{first_die}."
+  @outcome = "You rolled a #{first_die}."
 
-  "<h1>1d20</h1>
-  <p>#{outcome}</p>"
+  erb(:one_twenty)
+
 end
 
 get("/dice/5/4") do
@@ -53,9 +53,8 @@ get("/dice/5/4") do
   fourth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die
 
-  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, and a #{fourth_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, and a #{fourth_die} for a total of #{sum}."
 
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+  erb(:five_four)
 
 end
